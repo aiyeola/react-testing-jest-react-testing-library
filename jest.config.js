f8f897d4950ed3939be7ef22905fde7741852594
Victor Aiyeola <aiyeolavictor@gmail.com>
2022-05-08 04:05:52 +0100

    extend config for only eslint

--- jest.config.js
+++ jest.config.js
@@ -10,10 +10,10 @@ module.exports = {
   setupFilesAfterEnv: ['./src/jest.setup.ts'],
   coverageThreshold: {
     global: {
-      branches: 10,
-      functions: 10,
-      lines: 10,
-      statements: 100,
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
     },
   },
 };
